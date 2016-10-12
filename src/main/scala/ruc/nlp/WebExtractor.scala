9f7712c5753eae3f40c25c89541336c2f76ae4a6
xiatian <xiat@ruc.edu.cn>
2016-10-12 13:36:49 +0800

    add fingerprint and sentiment

--- src/main/scala/ruc/nlp/WebExtractor.scala
+++ src/main/scala/ruc/nlp/WebExtractor.scala
@@ -1,5 +1,6 @@
 package ruc.nlp
 
+import org.zhinang.extract.ExtractorConf
 import org.zhinang.extract.auto.{ArticleExtractor, Article}
 import org.zhinang.extract.auto.impl.ArticleExtractorImpl
 
@@ -9,14 +10,16 @@ import org.zhinang.extract.auto.impl.ArticleExtractorImpl
   * @author <a href="mailto:xiat@ruc.edu.cn">Tian Xia</a>
   */
 object WebExtractor {
+  val conf = ExtractorConf.create.setBoolean("extractor.mining.sentiment", true)
+
   def extractArticle(url: String): Article = {
-    val articleExtractor:ArticleExtractor = new ArticleExtractorImpl(new org.zhinang.conf.Configuration)
+    val articleExtractor:ArticleExtractor = new ArticleExtractorImpl(conf)
     articleExtractor.extracting(url)
     articleExtractor.getArticle
   }
 
   def extractArticle(url: String, content:String): Article = {
-    val articleExtractor:ArticleExtractor = new ArticleExtractorImpl(new org.zhinang.conf.Configuration)
+    val articleExtractor:ArticleExtractor = new ArticleExtractorImpl(conf)
     articleExtractor.extracting(url, content)
     articleExtractor.getArticle
   }
