2a327dc620fd263f5369f043b985581bdceb6e24
xiatian <xiat@ruc.edu.cn>
2016-10-13 10:43:18 +0800

    add mining api

--- src/main/scala/HTTP.scala
+++ src/main/scala/HTTP.scala
@@ -1,7 +1,5 @@
 import java.util.concurrent.Executors
 
-import scala.xml.XML
-
 import org.http4s.MediaType.{`text/html`, `text/xml`}
 import org.http4s._
 import org.http4s.dsl._
@@ -10,8 +8,9 @@ import org.http4s.server.blaze._
 import org.http4s.server.{Server, ServerApp}
 import ruc.nlp._
 
-import scalaz.concurrent.Task
 import scala.concurrent.duration._
+import scala.xml.XML
+import scalaz.concurrent.Task
 
 
 object MyService {
@@ -28,36 +27,11 @@ object ExtractQueryParamMatcher extends QueryParamDecoderMatcher[String]("url")
 object ExtractService {
   implicit val scheduledThreadPool = Executors.newScheduledThreadPool(5)
 
-  def extractArticleByUrlTask(url: String): Task[String] = Task {
-    println("fetching " + url)
-    val article = WebExtractor.extractArticle(url)
-    article.toXML
-  }.timed(5 second).handleWith {
-    case e: java.util.concurrent.TimeoutException => Task.now((<error>{e}</error>) toString)
-    case e: Throwable => Task.now((<error>{e}</error>) toString)
-  }
-
-  def extractArticleByContentTask(url: String, content:String): Task[String] = Task {
-    println(s"fetching $url by provided content...")
-    val article = WebExtractor.extractArticle(url, content)
-    article.toXML
-  }.timed(3 second).handleWith {
-    case e: java.util.concurrent.TimeoutException => Task.now((<error>{e}</error>) toString)
-    case e: Throwable =>
-      Task.now({
-        <error>
-          <message>{e}</message>
-          <url>{url}</url>
-          <content>{content}</content>
-        </error>
-      }.toString)
-  }
-
   /**
     * 执行挖掘任务，要求输入的文档内容格式为：
     * <article>
-    *   <title>中国人民大学信息资源管理学院</title>
-    *   <content>中国人民大学在中关村大街59号</content>
+    * <title> 三星Note7“炸机门”仍在发酵</title>
+    * <content><![CDATA[ 9月24日下午有网友爆料称，从苏宁购买的国行三星Note7发生了爆炸事故...  ]]></content>
     * </article>
     */
   def miningArticleTask(body: String): Task[String] = Task {
@@ -65,10 +39,17 @@ object ExtractService {
     val title = (doc \\ "title").text
     val content = (doc \\ "content").text
 
-    {<result>
-        <keyword>{title}</keyword>
-        <finger>233</finger>
-        <sentiment>0.2</sentiment>
+    {
+      <result>
+        <keyword>
+          {WebExtractor.extractKeywords(title, content)}
+        </keyword>
+        <finger>
+          {WebExtractor.fingerprint(title, content)}
+        </finger>
+        <sentiment>
+          {WebExtractor.sentiment(title, content)}
+        </sentiment>
       </result>
     }.toString
   }.timed(2 second).handleWith {
@@ -88,39 +69,82 @@ object ExtractService {
       }.toString)
   }
 
+
+  def extractArticleByContentTask(url: String, content: String): Task[String] = Task {
+    println(s"fetching $url by provided content...")
+    val article = WebExtractor.extractArticle(url, content)
+    article.toXML
+  }.timed(3 second).handleWith {
+    case e: java.util.concurrent.TimeoutException => Task.now((<error>
+      {e}
+    </error>) toString)
+    case e: Throwable =>
+      Task.now({
+        <error>
+          <message>
+            {e}
+          </message>
+          <url>
+            {url}
+          </url>
+          <content>
+            {content}
+          </content>
+        </error>
+      }.toString)
+  }
+
+  def extractArticleByUrlTask(url: String): Task[String] = Task {
+    println("fetching " + url)
+    val article = WebExtractor.extractArticle(url)
+    article.toXML
+  }.timed(5 second).handleWith {
+    case e: java.util.concurrent.TimeoutException => Task.now((<error>
+      {e}
+    </error>) toString)
+    case e: Throwable => Task.now((<error>
+      {e}
+    </error>) toString)
+  }
+
   val service = HttpService {
     case req@GET -> Root =>
       Ok(Task {"""<p>接口：/api/extract?url=xxx</p>"""})
         .withContentType(Some(`Content-Type`(`text/html`)))
 
-    case request@POST -> Root / "extract" =>
+    case request@POST -> Root / "extract" => {
       //val inputStream = scalaz.stream.io.toInputStream(request.body)
-      val body:String = EntityDecoder.decodeString(request).run //获取传递的内容
+      val body: String = EntityDecoder.decodeString(request).run //获取传递的内容
       //按行分割
       val lines = body.split("\n").toList
       lines match {
         //如果以http开始
-        case x::xs if x.toLowerCase.startsWith("http") => Ok(extractArticleByContentTask(x, xs.mkString("\n")).run)
+        case x :: xs if x.toLowerCase.startsWith("http") => Ok(extractArticleByContentTask(x, xs.mkString("\n")).run)
 
-        case _ => Ok( Task.now({<error>
-          <message>"""说明：POST的文本内容格式，第一行为URL地址，后面为该URL对应的网页源代码，例如：
-          http://www.test.com/article01.html
-          <html>
-            html content...
-          </html>
-          """
-          </message>
-          <received>{body}</received>
-        </error>} toString))
+        case _ => Ok(Task.now({
+          <error>
+            <message>"""说明：POST的文本内容格式，第一行为URL地址，后面为该URL对应的网页源代码，例如：
+              http://www.test.com/article01.html
+
+              <html>
+                html content...
+              </html>
+              """
+            </message>
+            <received>
+              {body}
+            </received>
+          </error>
+        } toString))
       }
-
+    }
     case request@GET -> Root / "extract" :? ExtractQueryParamMatcher(url) =>
       Ok(extractArticleByUrlTask(url).run)
         .putHeaders(`Content-Type`(`text/xml`))
 
-      //根据传入的XML格式的文章标题和正文，进行关键词提取/指纹处理等任务。
+    //根据传入的XML格式的文章标题和正文，进行关键词提取/指纹处理等任务。
     case request@POST -> Root / "mining" =>
-      val body:String = EntityDecoder.decodeString(request).run //获取传递的内容
+      val body: String = EntityDecoder.decodeString(request).run //获取传递的内容
       Ok(miningArticleTask(body).run)
         .putHeaders(`Content-Type`(`text/xml`))
 
--- src/main/scala/ruc/nlp/WebExtractor.scala
+++ src/main/scala/ruc/nlp/WebExtractor.scala
@@ -3,15 +3,19 @@ package ruc.nlp
 import org.zhinang.extract.ExtractorConf
 import org.zhinang.extract.auto.{ArticleExtractor, Article}
 import org.zhinang.extract.auto.impl.ArticleExtractorImpl
+import org.zhinang.nlp.keyword.KeywordExtractor
+import org.zhinang.nlp.similarity.Fingerprint
+import org.zhinang.nlp.sentiment.SentimentParser
 
 /**
-  * 正文自动抽取的Scala包装器
+  * 自动抽取的Scala包装器
   * 
   * @author <a href="mailto:xiat@ruc.edu.cn">Tian Xia</a>
   */
 object WebExtractor {
   val conf = ExtractorConf.create
     .setBoolean("extractor.mining.sentiment", true)
+    .setBoolean("nlp.keyword.show.tf", true)
     .setInt("extractor.keywords.topN", 15)
 
   def extractArticle(url: String): Article = {
@@ -24,5 +28,19 @@ object WebExtractor {
     val articleExtractor:ArticleExtractor = new ArticleExtractorImpl(conf)
     articleExtractor.extracting(url, content)
     articleExtractor.getArticle
+  }
+
+  def extractKeywords(title: String, content: String) = {
+    val keywordExtractor = new KeywordExtractor(conf)
+    keywordExtractor.extractAsString(title, content, conf.getInt("extractor.keywords.topN", 10))
+  }
+
+  def fingerprint(title: String, content: String) = {
+    val keywordExtractor = new KeywordExtractor(conf, false)
+    Fingerprint.fingerprint(title, keywordExtractor.extractAsList(title, content, 5))
+  }
+
+  def sentiment(title:String, content: String) = {
+    SentimentParser.parse(conf, title).getSentiment*0.8 + SentimentParser.parse(conf, content).getSentiment*0.2
   }
 }
